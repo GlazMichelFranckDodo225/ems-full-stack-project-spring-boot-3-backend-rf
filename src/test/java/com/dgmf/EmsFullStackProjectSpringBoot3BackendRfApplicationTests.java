5f61a7068b144f6feb0cde47ba1bf0aceee72878
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-09 11:51:37 +0100

    Project Initialization

---
+++ src/test/java/com/dgmf/EmsFullStackProjectSpringBoot3BackendRfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsFullStackProjectSpringBoot3BackendRfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
